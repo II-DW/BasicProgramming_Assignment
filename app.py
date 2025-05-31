74badcacdf16797434f0f63b466e7f35f3c1714f
ii_dw <dowon206@kakao.com>
2025-05-31 15:28:33 +0900

    add: Database 관리 함수 및 오류 수정

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from utils.functions import chat
 
 if __name__ == '__main__':
     while True :
-        input_msg = input("프로그램 실행 목적을 입력하세요 (0: 종료, 1: 감정등록, 2: 감정캘린더확인)")
+        input_msg = input("프로그램 실행 목적을 입력하세요 (0: 종료, 1: 감정등록, 2: 감정캘린더확인) : ")
         if input_msg == "1" :
             chat()
         elif input_msg == "2" :
@@ -12,5 +12,5 @@ if __name__ == '__main__':
             print("프로그램을 종료합니다.")
             break
         else :
-            print("1이나 2 중 하나의 값을 입력해주시기 바랍니다.")
+            print("0, 1, 2 중 하나의 값을 입력해주시기 바랍니다.")
             continue
--- utils/functions.py
+++ utils/functions.py
@@ -6,6 +6,7 @@ import json
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
+feelings_L = ["기쁨", "두려움", "슬픔", "지침", "짜증", "혐오"]
 
 def load_feeling () :
     data = json.loads(Path('data.json').read_text())
@@ -13,12 +14,23 @@ def load_feeling () :
 
 def save_feeling (date, feeling) :
     data = json.loads(Path('data.json').read_text())
-    data.append({date: feeling})
-    with Path('data.json').oepn('w+t', encoding='utf8') as fp: json.dump(data, fp, indent=4, ensure_ascii=False)
+    date = str(date)
+    if date in data:
+        while True :
+            input_msg = input("이미 해당 일자에 저장된 날짜가 있습니다. 대체하시겠습니까? 원래 감정을 유지하겠습니까? (0: 대체, 1: 유지) : ")
+            if input_msg == "0" :
+                break
+            elif input_msg == "1" :
+                return
+            else :
+                print("제대로 다시 입력하십시오")
+                continue
+    data[date] = feeling
+    with Path('data.json').open('w+t', encoding='utf8') as fp: json.dump(data, fp, indent=4, ensure_ascii=False)
     print("감정이 저장되었습니다!")
     
 
-def load_ai (diary_text) : # 참고: https://wikidocs.net/217882
+def load_ai (diary_text) :
     system_prompt = """
     You are an emotion analyzer.
     Read the given diary entry and evaluate the scores of six emotions as floating-point numbers between 0.0 and 1.0.
@@ -36,34 +48,45 @@ def load_ai (diary_text) : # 참고: https://wikidocs.net/217882
     - Only provide the analysis. Do not give any explanations or additional output.
     """
     
-    client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-    )
-
-    try :
-        completion = client.chat.completions.create(
-        extra_headers={
-            "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-            "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-        },
-        extra_body={},
-        model="deepseek/deepseek-r1-0528:free",
-        messages=[
-            {
-                "role": "system",
-                "content": system_prompt
-            },
-            {
-            "role": "user",
-            "content": diary_text
-            }
-        ]
-        )
-        return completion.choices[0].message.content
-    except Exception as e:
-        print(f"에러 발생: {e}")
-        return "⚠️ 감정 분석에 실패했습니다. API 제한을 확인해주세요."
+    # client = OpenAI(
+    # base_url="https://openrouter.ai/api/v1",
+    # api_key=api_key,
+    # )
+    # while True :
+    #     try :
+    #         completion = client.chat.completions.create(
+    #         extra_headers={
+    #             "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    #             "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+    #         },
+    #         extra_body={},
+    #         model="deepseek/deepseek-r1-0528:free",
+    #         messages=[
+    #             {
+    #                 "role": "system",
+    #                 "content": system_prompt
+    #             },
+    #             {
+    #             "role": "user",
+    #             "content": diary_text
+    #             }
+    #         ]
+    #         )
+    #         try:
+    #             response = [float(feelings.split(':')[1]) for feelings in completion.choices[0].message.content.split("\n")]
+    #         except Exception as e :
+    #             print(f"에러 발생: {e}")
+    #             print("⚠️ LLM이 제대로 된 값을 제공하지 않았습니다. 재시도 중입니다.")
+    #             continue
+    #         if len(response) != 6 :
+    #             print("⚠️ LLM이 제대로 된 값을 제공하지 않았습니다. 재시도 중입니다.")
+    #             continue
+    #         return response
+    #     except Exception as e:
+    #         print(f"에러 발생: {e}")
+    #         return "⚠️ 감정 분석에 실패했습니다. API 제한을 확인해주세요."
+    return [0.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+    
     
 
 def chat () :
@@ -82,7 +105,9 @@ def chat () :
             continue
         
         feeling_msg = input("오늘 어떤 일이 있었나요? \n")
-        print(load_ai(feeling_msg))
+        response = load_ai(feeling_msg)
+        
+        save_feeling(date, feelings_L[response.index(max(response))])
         
         
         
